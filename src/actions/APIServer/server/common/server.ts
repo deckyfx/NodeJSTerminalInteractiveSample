260db1ba00c96ba369c8f576d1090380c7e401ab
Decky Fiyemonda <decky.fx@gmail.com>
2019-01-09 18:26:00 +0700

    Fix Server static files

--- src/actions/APIServer/server/common/server.ts
+++ src/actions/APIServer/server/common/server.ts
@@ -5,18 +5,23 @@ import * as bodyParser from 'body-parser';
 import * as http from 'http';
 import * as os from 'os';
 import * as cookieParser from 'cookie-parser';
+import * as serveIndex from 'serve-index';
 import Util from '../../../../Util';
 
 const app = express();
 
 export default class ExpressServer {
     constructor() {
-    const root = path.normalize(__dirname + '/../..');
+        const root = path.normalize(__dirname + '/..');
         app.set('appPath', root + 'client');
         app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }));
         app.use(bodyParser.urlencoded({ extended: true, limit: process.env.REQUEST_LIMIT || '100kb' }));
         app.use(cookieParser(process.env.SESSION_SECRET));
-        app.use(express.static(`${root}/public`));
+        app.use(express.static(`${root}/public`), serveIndex(`${root}/public`, {'icons': true}));
+        app.use('/public', express.static(`${root}/public`), serveIndex(`${root}/public`, {'icons': true}));
+        app.use('/js', express.static(`${root}/public/js`), serveIndex(`${root}/public/js`, {'icons': true}));
+        app.use('/css', express.static(`${root}/public/css`), serveIndex(`${root}/public/css`, {'icons': true}));
+        app.use("/image", express.static(`${root}/public/image`), serveIndex(`${root}/public/image`, {'icons': true}));
     }
 
     router(routes: (app: Application) => void): ExpressServer {
--- src/actions/APIServer/server/routes.ts
+++ src/actions/APIServer/server/routes.ts
@@ -4,11 +4,10 @@ import * as path from 'path';
 import examplesRouter from './api/controllers/examples/router';
 
 const publicdir: string = path.join(__dirname, 'public');
+const root = path.normalize(__dirname + '/../..');
+
+console.log(publicdir, root);
 
 export default function routes(app: Application): void {
     app.use('/api/v1/examples', examplesRouter);
-
-    app.get('/', function (req, res) {
-        res.sendFile(path.join(publicdir, 'index.html'));
-    })
 };
